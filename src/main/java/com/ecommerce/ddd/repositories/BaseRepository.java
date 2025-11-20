360a199bc940fb7088db40ad591f19dcf3cb8b19
mcalsing <marcelocalsing@gmail.com>
2025-11-20 10:55:00 -0300

    feat: iniciado frontend

--- src/main/java/com/ecommerce/ddd/repositories/BaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.ddd.repositories;
-
-import com.ecommerce.ddd.models.Base;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BaseRepository extends JpaRepository<Base, Long> {
-}
